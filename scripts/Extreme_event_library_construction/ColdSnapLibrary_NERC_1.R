52d097e0e5b831148cb868548d4dcfa4c5158cfb
Alfred <73004247+crystalandwan@users.noreply.github.com>
2025-10-01 11:34:43 -0700

    Update ColdSnapLibrary_NERC_1.R

--- scripts/Extreme_event_library_construction/ColdSnapLibrary_NERC_1.R
+++ scripts/Extreme_event_library_construction/ColdSnapLibrary_NERC_1.R
@@ -1,7 +1,7 @@
 library(tools)
 
 # Set working directory
-setwd("C:/Users/wanh535/OneDrive - PNNL/Desktop/IM3/Heat Waves/Climate_data/historic")
+setwd("PATH_TO_NERC_LEVEL_CLIMATE_DATA")
 
 # Create the directory for heat wave library files if it does not exist
 output_dir <- "cold_snap_library"
@@ -116,3 +116,4 @@ for (scenario in scenarios) {
     }
   }
 }
+
