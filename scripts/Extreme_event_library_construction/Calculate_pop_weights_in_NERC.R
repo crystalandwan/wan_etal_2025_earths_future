878981a510684f196d052ae41e2fc12f110b26d1
Alfred <73004247+crystalandwan@users.noreply.github.com>
2025-10-01 17:42:34 -0700

    Add files via upload

---
+++ scripts/Extreme_event_library_construction/Calculate_pop_weights_in_NERC.R
@@ -0,0 +1,37 @@
+### This script converts the county-level population in 2019 to the weights (between 0 and 1)
+### within each NERC subregions.
+
+library(data.table)
+library(dplyr)
+library(sf)
+
+setwd("C:/Users/wanh535/OneDrive - PNNL/Desktop/IM3/Heat Waves")
+
+## Read in county-NERC relation shapefile
+counties <- st_read("./Climate_data/counties_in_NERC2020.shp")
+# Create FIPS by removing the leading 0
+counties$FIPS <- counties$GEOID
+counties$FIPS <- gsub("^0+", "", counties$FIPS)
+counties$FIPS <- as.numeric(counties$FIPS)
+
+## Read in population data
+pop <- fread("./pop_data/county_populations_2000_to_2019.csv")
+# Shannon county (FIPS 46113) in SD was renamed as Oglala Lakota county (FIPS 46102) in 2015.
+# In order to match the join with the county shapefile, rename the FIPS
+pop[pop$county_FIPS == 46113, "county_FIPS"] <- 46102
+
+## Join counties with pop
+join <- merge(counties, pop[, c("county_FIPS", "pop_2019")], 
+              by.x = "FIPS", by.y = "county_FIPS", all.x = TRUE)
+
+## Calculate the weight of each county's population within its NERC subregion.
+join <- join %>%
+  group_by(ID) %>%
+  mutate(total_pop_region = sum(pop_2019, na.rm = TRUE)) %>%
+  ungroup() %>% 
+  mutate(pop_weight = pop_2019/total_pop_region)
+
+## Write out the results
+join2 <- st_drop_geometry(join)
+join2 <- join2[, c("FIPS", "pop_weight")]
+fwrite(join2, "./pop_data/county_pop2019_weights_in_NERC.csv")
